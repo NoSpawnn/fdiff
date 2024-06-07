386cd8f2259611eb65ddc5fa0f170d8143106b27
NoSpawnn <adam.blighe@protonmail.com>
2024-06-07 16:05:55 +0100

    Small refactoring, change input status whenever `submitted` is updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,40 +13,32 @@
   let right_text_diff = "";
   let submitted = false;
 
-  function setInputState() {
-    let inputs = document.getElementsByClassName("text-input");
-    let submitted = document.getElementsByClassName("text-submitted");
+  let input_areas = document.getElementsByClassName("text-input");
+  let submitted_areas = document.getElementsByClassName("text-submitted");
 
-    for (let i = 0; i < inputs.length; i++) {
-      let input_elem = inputs.item(i) as HTMLTextAreaElement;
+  $: submitted, setInputState();
+
+  function setInputState() {
+    for (let i = 0; i < input_areas.length; i++) {
+      let input_elem = input_areas.item(i) as HTMLTextAreaElement;
       input_elem.style.display = submitted ? "none" : "block";
 
-      let submit_elem = submitted.item(i) as HTMLDivElement;
-      submit_elem.style.display = submitted ? "block" : "none";
+      let submitted_elem = submitted_areas.item(i) as HTMLDivElement;
+      submitted_elem.style.display = submitted ? "block" : "none";
     }
   }
 
-  async function submit() {
+  async function getDiff() {
     submitted = true;
-    setInputState();
 
     invoke("get_diff", {
       left_text: left_text_input,
       right_text: right_text_input,
     }).then((response) => {
-      left_text_diff = "";
-      right_text_diff = "";
-
       (response as LineDiff[]).forEach((diffEntry) => {
         if (diffEntry.is_diff) {
-          left_text_diff +=
-            '<span style="background-color: var(--text-diff-bg-color);">' +
-            diffEntry.left_text +
-            "</span><br>";
-          right_text_diff +=
-            '<span style="background-color: var(--text-diff-bg-color);">' +
-            diffEntry.right_text +
-            "</span><br>";
+          left_text_diff += `<span style="background-color: var(--text-diff-bg-color);">${diffEntry.left_text}</span><br>`;
+          right_text_diff += `<span style="background-color: var(--text-diff-bg-color);">${diffEntry.right_text}</span><br>`;
         } else {
           left_text_diff += diffEntry.left_text + "<br>";
           right_text_diff += diffEntry.right_text + "<br>";
@@ -55,9 +47,16 @@
     });
   }
 
-  async function unsubmit() {
+  function editInput() {
     submitted = false;
-    setInputState();
+  }
+
+  function clearText() {
+    left_text_diff = "";
+    right_text_diff = "";
+    left_text_input = "";
+    right_text_input = "";
+    submitted = false;
   }
 </script>
 
@@ -73,11 +72,16 @@
     </div>
   </div>
 
-  {#if submitted}
-    <button class="text-submit" type="submit" on:click={unsubmit}>Edit</button>
-  {:else}
-    <button class="text-submit" type="submit" on:click={submit}>Diff</button>
-  {/if}
+  <div class="buttons-container">
+    {#if submitted}
+      <button class="text-submit" type="submit" on:click={editInput}
+        >Edit</button
+      >
+    {:else}
+      <button class="text-submit" type="submit" on:click={getDiff}>Diff</button>
+    {/if}
+    <button class="text-submit" on:click={clearText}>Clear</button>
+  </div>
 </div>
 
 <style>
@@ -114,7 +118,6 @@
     width: 45vw;
     font-size: 15px;
     padding: 0.3rem;
-    color: var(--text-color);
     background-color: var(--secondary-bg-color);
   }
 
@@ -130,9 +133,15 @@
     padding: 0.3rem;
     font-size: 15px;
     width: 45vw;
-    color: var(--text-color);
     background-color: var(--secondary-bg-color);
     white-space: pre-wrap;
+    overflow: scroll;
+  }
+
+  .buttons-container {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
   }
 
   button {
@@ -141,7 +150,10 @@
     padding: 0.4rem;
     width: 10rem;
     background-color: var(--secondary-bg-color);
-    color: var(--text-color);
     border: solid 2px var(--active-outline-color);
+  }
+
+  button:hover {
+    cursor: pointer;
   }
 </style>
