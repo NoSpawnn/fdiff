67dc8a3889086cfbc36b8f84e709c067b971a811
NoSpawnn <adam.blighe@protonmail.com>
2024-06-07 15:34:43 +0100

    Implement diff function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,69 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
+#[derive(serde::Serialize)]
+struct LineDiff {
+    left_text: String,
+    right_text: String,
+    is_diff: bool,
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn submit(text1: &str, text2: &str) -> String {
-    println!("Text 1: {text1}");
-    println!("Text 2: {text2}");
-    format!("{text1}\n{text2}")
+fn get_diff(left_text: &str, right_text: &str) -> Vec<LineDiff> {
+    let mut line_diffs: Vec<LineDiff> = Vec::new();
+    let mut text1_lines = left_text.lines();
+    let mut text2_lines = right_text.lines();
+
+    loop {
+        match (text1_lines.next(), text2_lines.next()) {
+            (Some(x), Some(y)) => {
+                println!("left={x}, right={y}");
+
+                line_diffs.push(LineDiff {
+                    left_text: x.to_string(),
+                    right_text: y.to_string(),
+                    is_diff: x != y,
+                })
+            }
+            (Some(x), None) => {
+                println!("left={x}, right=none");
+
+                line_diffs.push(LineDiff {
+                    left_text: x.to_string(),
+                    right_text: "".to_string(),
+                    is_diff: true,
+                })
+            }
+            (None, Some(y)) => {
+                println!("right={y}, right=none");
+
+                line_diffs.push(LineDiff {
+                    left_text: "".to_string(),
+                    right_text: y.to_string(),
+                    is_diff: true,
+                })
+            }
+            (None, None) => break,
+        }
+    }
+
+    return line_diffs;
 }
 
 fn main() {
-
-
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![submit])
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![get_diff])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
